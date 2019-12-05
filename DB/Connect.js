4cff2ef3faff3984d6a513db0713952f4c6b5ffc
amitg269 <amitgo.92@gmail.com>
2019-12-05 15:52:04 +0530

    added changes

--- DB/Connect.js
+++ DB/Connect.js
@@ -1,16 +1,25 @@
 const mongodb= require('mongodb');
+const Logger=require('../Logger.js')
 const MongoClient = mongodb.MongoClient;
-
+let db;
 
 
 const MongoConnect=(callback)=>{
     MongoClient.connect('mongodb+srv://amitg269:SbXdNaHeEQUHKnea@restro-nkntu.mongodb.net/test?retryWrites=true&w=majority',{useUnifiedTopology: true}).then(client=>{
         console.log('Connected!');
-        callback(client);
+        db=client;
     }).catch(err=>{
-      console.log(err);
+     Logger.errorLogger("MongoConnect","Connect.js",err.stack,"connection failed");
+     
     })
 }
 
+
+const getDB=()=>{
+    if(db!=null){
+        return db;
+    }
+    return "Connection Not Done";
+}
 
 module.exports=MongoConnect;
