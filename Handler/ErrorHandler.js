bf836c4dfbc2d104646c0f41c8c24913c43a5304
amitg269 <amitgo.92@gmail.com>
2019-12-08 13:20:01 +0530

    added changes

--- Handler/ErrorHandler.js
+++ Handler/ErrorHandler.js
@@ -2,14 +2,47 @@ var express=require('express');
 var app=express();
 const viewErrorInfo=require('../Common/Response.js');
 const bodyParser = require('body-parser');
+const createResponse=require('../Common/Response.js');
+const Enum=require('../Enum.js');
 app.use(bodyParser.json())
 
 
 
 app.post("/viewerrors",function(req,res,next){
 
+   var response;
     var data=viewErrorInfo.viewErros();
-    res.body=data;
+    if(data.Success==true && data.Data!="")
+    {
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.Success,Enum.ReturnMsg.Success);
+    }
+    else if(data.Success==true && data.Data==""){
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.NoRecordFound,Enum.ReturnMsg.NoRecordFound);
+    }
+    else if(data.Success==false && data.Data==""){
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.Exception,Enum.ReturnMsg.Exception);
+    }
+    res.body=response;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(res.body));
+   
+});
+
+app.post("/infologs",function(req,res,next){
+
+    var response;
+    var data=viewErrorInfo.viewInfo();
+    if(data.Success==true && data.Data!="")
+    {
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.Success,Enum.ReturnMsg.Success);
+    }
+    else if(data.Success==true && data.Data==""){
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.NoRecordFound,Enum.ReturnMsg.NoRecordFound);
+    }
+    else if(data.Success==false && data.Data==""){
+        response= createResponse.createResponse(data.Data,Enum.ReturnCode.Exception,Enum.ReturnMsg.Exception);
+    }
+    res.body=response;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(res.body));
    
